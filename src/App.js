25ddf024b8a0326130384dcf30c5f36827a65aae
rakshitha-siriwardhana <61771242+Rakshithasiriwardhana@users.noreply.github.com>
2024-04-10 12:26:48 +0530

    navigation and firebase setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Directory from './components/directory/directory.component';
 import { Routes, Route } from 'react-router-dom';
+
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation';
+import SignIn from './routes/SignIn/SignIn';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };
