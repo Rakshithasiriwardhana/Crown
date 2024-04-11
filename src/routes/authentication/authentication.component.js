368effbef505db989d31acd036e6995d9c9ba584
rakshitha-siriwardhana <61771242+Rakshithasiriwardhana@users.noreply.github.com>
2024-04-11 12:00:05 +0530

    created authentication page and sign in form and sign up

--- src/routes/authentication/authentication.component.js
+++ src/routes/authentication/authentication.component.js
@@ -1,6 +1,5 @@
-import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
-import SignInForm from '../../components/sign-in-form/sign-in-form.component';
-
+import SignUpForm from '../../components/sign-up/SignUp';
+import SignInForm from '../../components/sign-in/SignIn';
 import './authentication.styles.scss';
 
 const Authentication = () => {
