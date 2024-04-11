d61673963083f700ef7d7781bca88ffc81e9ab13
rakshitha-siriwardhana <61771242+Rakshithasiriwardhana@users.noreply.github.com>
2024-04-11 11:19:22 +0530

    created authentication page for sign up and signin form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import Directory from './components/directory/directory.component';
 import { Routes, Route } from 'react-router-dom';
-
-import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation';
+import Home from './routes/home/home.component';
 import SignIn from './routes/SignIn/SignIn';
+
+import Authentication from './routes/authentication/authentication.component';
+
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
 };
-
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
